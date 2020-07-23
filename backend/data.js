a7b192432447bb94bb2027edfea794ce8eb41e9b
Akash Shaw <aakashshaw88@gmail.com>
2020-07-23 10:33:46 +0530

    backend

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+export default  {
+    products : [
+        {
+        id: '1',
+        name: 'Milkfood',
+        description: 'Milkfood for',
+        image: '/images/d1.jpg',
+        quantity: '1L',
+        offer: '18%',
+        MRP: '599',
+        price: '590'
+    },
+    {   
+        id: '2',
+        name: 'Sweet fresh stawberry',
+        description: 'Sweet fresh stawberry on the wooden table',
+        image: '/images/d2.jpg',
+        quantity: '2 packets',
+        offer: '20',
+        MRP: '720',
+        price: '600'
+    },
+    {   
+        id: '3',
+        name: 'Baking cake',
+        description: 'Baking cake in rural kitchen - dough  recipe ingredients (eggs, flour, sugar) on vintage wooden table from above.',
+        image: '/images/d3.jpg',
+        quantity: '1L',
+        offer: '15',
+        MRP: '899',
+        price: '650'
+    },
+    {   
+        id: '4',
+        name: 'Patanjaili Ghee',
+        description: 'Grofers Mother choice Ghee',
+        image: '/images/d4.jpg',
+        quantity: '500gms',
+        offer: '12',
+        MRP: '1490',
+        price: '1299'
+    },
+    {   
+        id: '5',
+        name: 'Honey',
+        description: 'Honey and honeycell on the table',
+        image: '/images/d5.jpg',
+        quantity: '500 ml',
+        offer: '20',
+        MRP: '950',
+        price: '890'
+    },
+    {   
+        id: '6',
+        name: 'Yogurt',
+        description: 'Homemade yogurt with raspberry and mint',
+        image: '/images/d6.jpg',
+        quantity: '250 ml',
+        offer: '10',
+        MRP: '499',
+        price: '300'
+    },
+]
+}
